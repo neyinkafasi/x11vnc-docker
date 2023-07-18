a2518dbd827fe941dc68c91f7828ce252aaa6f6b
neyinkafasi <119981734+neyinkafasi@users.noreply.github.com>
2023-07-18 22:04:12 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,9 @@
 #! /bin/sh
 
-Xvfb :99 -ac -listen tcp -screen 0 2560x1440x24 &
+Xvfb :99 -ac -listen tcp -screen 0 800x600x16 &
 sleep 3
 /usr/bin/fluxbox -display :99 -screen 0 &
 sleep 3
 x11vnc -display :99.0 -forever -passwd ${X11VNC_PASSWORD:-password} 
+sleep 5
+/usr/bin/peer2profit
